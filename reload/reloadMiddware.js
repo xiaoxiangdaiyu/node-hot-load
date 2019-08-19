f316d01e252df903d29af0c8ade025cfd5725e41
xiaoxiangdaiyu <935829048@qq.com>
2019-08-19 20:21:55 +0800

    更新 语句

--- reload/reloadMiddware.js
+++ reload/reloadMiddware.js
@@ -1,8 +1,10 @@
 //中间件，实现监听文件及重启操作
-var watch = require('watch')
+var chokidar = require('chokidar');
 var cp = require('child_process')
 var path = require('path')
 var appIns = cp.fork(path.join(__dirname, './src/app.js'));
+var watch = null
+
 function handelChange(){
     appIns.kill('SIGINT')
     return cp.fork(path.join(__dirname, './src/app.js'))
@@ -10,11 +12,13 @@ function handelChange(){
 
 function wacthDir(app,dir){
     watch.watchTree(__dirname + '/views',function(f,cur,prev){
-        // 非初始
-        if (!!prev){
-            console.log('in change')
-            appIns = handelChange('isChange') 
+        if (!watch) {
+            watch = chokidar.watch(dir)
         }
+        watch.on('change', function (path) {
+            console.log('file change>>>', path)
+            appIns = handelChange('isChange')
+        });
     })  
 }
 process.on('SIGINT', () => {
